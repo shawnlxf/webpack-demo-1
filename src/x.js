e7c5629e4de2414b632815defcbeee53d2703ceb
shawn <1838435618@qq.com>
2020-10-24 21:03:25 +0800

    update

--- src/x.js
+++ src/x.js
@@ -1,3 +1,3 @@
-
+import './x.css'
 export default 'xxx'
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,24 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 var path = require('path');
 
 module.exports = {
-  mode: 'development',
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].[contenthash].js'
-  }
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'index.[contenthash].js'
+    },
+    plugins: [new HtmlWebpackPlugin({
+        title: 'XDML - 写代码啦',
+        template: 'src/assets/index.html'
+    })],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
 };
+
