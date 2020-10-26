ea41f0c7e38ad761bf31e77c6c1f88f4ce13ea88
shawn <1838435618@qq.com>
2020-10-26 17:26:51 +0800

    update

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -12,6 +12,51 @@ module.exports = {
             title: 'XDML - 写代码啦',
             template: 'src/assets/index.html'
         })
-    ]
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                    },
+                ],
+            },
+            {
+                test: /\.styl$/,
+                loader: [
+                    // Creates `style` nodes from JS strings
+                    'style-loader',
+                    // Translates CSS into CommonJS
+                    'css-loader',
+                    // compiles Styl to CSS
+                    'stylus-loader',
+                ]
+            },
+            {
+                test: /\.less$/,
+                loader: [
+                    // Creates `style` nodes from JS strings
+                    'style-loader',
+                    // Translates CSS into CommonJS
+                    'css-loader',
+                    // compiles Less to CSS
+                    'less-loader',
+                ]
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    'style-loader',
+                    // Translates CSS into CommonJS
+                    'css-loader',
+                    // Compiles Sass to CSS
+                    'sass-loader',
+                ],
+            },
+        ],
+    },
 };
 
